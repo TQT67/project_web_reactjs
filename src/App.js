601d632084eace0ff177f8ab2911804865d1e2b3
TQT67 <trangtrinh000000@gmail.com>
2023-03-21 23:35:13 +0700

    cập_nhật_full

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import { BrowserRouter } from "react-router-dom";
-import Signup from "./components/Signup/Signup";
-
-
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+import store from "./store/store";
+import { Provider } from "react-redux";
+import {
+  CartPage,
+  CategoryProductPage,
+  HomePage,
+  ProductSinglePage,
+  SearchPage,
+} from "./pages/index.jsx";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Signup/>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Sidebar />
+
+          <Routes>
+            {/* home page route */}
+            <Route path="/" element={<HomePage />} />
+            {/* single product route */}
+            <Route path="/product/:id" element={<ProductSinglePage />} />
+            {/* category wise product listing route */}
+            <Route path="/category/:category" element={<CategoryProductPage />} />
+            {/* cart */}
+            <Route path="/cart" element={<CartPage />} />
+            {/* searched products */}
+            <Route path="/search/:searchTerm" element={<SearchPage />} />
+          </Routes>
+
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
